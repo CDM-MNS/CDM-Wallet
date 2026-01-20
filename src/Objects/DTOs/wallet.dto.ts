54f296af8a3b56558f45fd7a6f98baa7f3fe75ae
Ethan-C11 <ethan.collignon.1102@gmail.com>
2026-01-20 11:25:16 +0100

    fix: removed id from dto

--- src/Objects/DTOs/wallet.dto.ts
+++ src/Objects/DTOs/wallet.dto.ts
@@ -1,13 +1,11 @@
 
 export class WalletDto {
-    id: number;
     ownerId: number;
     coinsBalance: number;
     freeCoinsBalance: number;
 
-    constructor(id: number, ownerId: number, coinsBalance: number, freeCoinsBalance: number)
+    constructor(ownerId: number, coinsBalance: number, freeCoinsBalance: number)
     {
-        this.id = id;
         this.ownerId = ownerId;
         this.coinsBalance = coinsBalance;
         this.freeCoinsBalance = freeCoinsBalance;
