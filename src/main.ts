8501aec1ea8b6837a282034d099f628c8d621a30
Ethan-C11 <ethan.collignon.1102@gmail.com>
2026-01-20 10:47:18 +0100

    feat: RMQ integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBIT_MQ ?? ''],
+      queue: 'WALLET_QUEUE_IN',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBIT_MQ ?? ''],
+      queue: 'WALLET_QUEUE_OUT',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
+  await app.startAllMicroservices()
+
+
   await app.listen(process.env.AP_PORT ?? 4006);
 }
 bootstrap();
