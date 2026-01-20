271ac8653feba5a3674f28356ab96f35bf549ed5
Ethan-C11 <ethan.collignon.1102@gmail.com>
2026-01-20 11:25:39 +0100

    feat: create wallet for user event handler

--- src/Controllers/wallet.controller.ts
+++ src/Controllers/wallet.controller.ts
@@ -2,36 +2,43 @@ import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
 import {WalletService} from "../Services/wallet.service";
 import {WalletDto} from "../Objects/DTOs/wallet.dto";
 import {DeleteResult, UpdateResult} from "typeorm";
+import {EventPattern, Payload} from "@nestjs/microservices";
+import {UserDto} from "@cdm/models";
 
 @Controller('wallet')
 export class WalletController {
 
-    constructor(private readonly userWalletService: WalletService) {
+    constructor(private readonly walletService: WalletService) {
     }
 
     @Get()
     async getAll() : Promise<WalletDto[]> {
-        return await this.userWalletService.getAll();
+        return await this.walletService.getAll();
     }
 
     @Get(':id')
     async getById(@Param('id') id: number) : Promise<WalletDto> {
-        return await this.userWalletService.getById(id);
+        return await this.walletService.getById(id);
     }
 
     @Post()
     async create(@Body() userWalletDto: WalletDto) : Promise<WalletDto> {
-        return await this.userWalletService.create(userWalletDto);
+        return await this.walletService.create(userWalletDto);
     }
 
     @Put(":id")
     async update(@Param('id') id:number, @Body() userWalletDto: WalletDto) : Promise<UpdateResult> {
-        return await this.userWalletService.update(id, userWalletDto);
+        return await this.walletService.update(id, userWalletDto);
     }
 
     @Delete(":id")
     async delete(@Param('id') id:number) : Promise<DeleteResult> {
-        return await this.userWalletService.delete(id)
+        return await this.walletService.delete(id)
+    }
+
+    @EventPattern('user_created')
+    async handleEventUserCreated(@Payload() data: UserDto) {
+        await this.walletService.createNewWalletForUser(data);
     }
 
 
--- src/Services/wallet.service.ts
+++ src/Services/wallet.service.ts
@@ -3,21 +3,22 @@ import {WalletEntity} from "../Objects/Entities/wallet.entity";
 import {WalletDto} from "../Objects/DTOs/wallet.dto";
 import {DeleteResult, Repository, UpdateResult} from "typeorm";
 import {Injectable, NotFoundException} from "@nestjs/common";
+import {UserDto} from "@cdm/models";
 
 @Injectable()
 export class WalletService {
 
     constructor(
         @InjectRepository(WalletEntity)
-        private userWalletRepository: Repository<WalletEntity>
+        private walletRepository: Repository<WalletEntity>
     ) {}
 
     async getAll() : Promise<WalletDto[]> {
-        return await this.userWalletRepository.find();
+        return await this.walletRepository.find();
     }
 
     async getById(id: number) : Promise<WalletDto> {
-        const fetchedWallet = await this.userWalletRepository.findOne({
+        const fetchedWallet = await this.walletRepository.findOne({
             where : { id : id }
         })
 
@@ -28,14 +29,22 @@ export class WalletService {
     }
 
     async create(userWalletDto: WalletDto) : Promise<WalletDto> {
-        return await this.userWalletRepository.save(userWalletDto);
+        return await this.walletRepository.save(userWalletDto);
     }
 
     async update(id: number, userWalletDto: WalletDto) : Promise<UpdateResult> {
-        return await this.userWalletRepository.update(id, userWalletDto)
+        return await this.walletRepository.update(id, userWalletDto)
     }
 
     async delete(id: number) : Promise<DeleteResult> {
-        return await this.userWalletRepository.delete(id);
+        return await this.walletRepository.delete(id);
+    }
+
+    async createNewWalletForUser(userDto: UserDto) : Promise<WalletDto> {
+        if(userDto.id === undefined)
+            throw new NotFoundException("User with id is missing");
+
+        const wallet = new WalletDto(0, userDto.id, 0, 50)
+        return await this.walletRepository.save(wallet);
     }
 }
